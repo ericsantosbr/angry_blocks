0933a62f9950b56d4540cfb6dc6024194f2c41d9
Eric Santos <ericsantosbr2@gmail.com>
2019-05-21 19:57:42 -0300

    Test character added

---
+++ testCharacter.lua
@@ -0,0 +1,24 @@
+testCharacter = {}
+
+testCharacter.newCharacter = function()
+	local newCharacter = {}
+
+	newCharacter.body = p.newBody(world, g.getWidth() / 2, g.getHeight() / 2, "dynamic")
+	newCharacter.shape = p.newRectangleShape(20, 20)
+	newCharacter.fixture = p.newFixture(newCharacter.body, newCharacter.shape)
+	newCharacter.color = {1, 0, 0, 1}
+	newCharacter.draw = function(self)
+		g.setColor(self.color)
+		g.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
+	end
+
+	newCharacter.applyForce = function(self, dx, dy) 
+		self.body:applyForce(dx, dy)
+	end
+
+
+	return newCharacter
+end
+
+
+return testCharacter
